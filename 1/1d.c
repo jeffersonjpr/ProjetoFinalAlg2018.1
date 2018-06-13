5461d977482e94d5da441619c201bd551785a7b1
jeffersonjpr <jefferson_michael407@hotmail.com>
2018-06-13 18:56:14 -0300

    1d

---
+++ 1/1d.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//funçao abaixo nao copie acima
+fib(int n){
+	if(n <= 1) return 0;
+	else if(n <= 3) return 1;
+	return fib(n-1) + fib(n-2);
+}
+//funçao acima nao copie abaixo
+
+int main(){ //casos testes
+	printf("%i\n",fib(258)); // funcionou
+	printf("%i\n",fib(1)); // funcionou
+	printf("%i\n",fib(2)); // funcionou
+	printf("%i\n",fib(3)); // funcionou
+	printf("%i\n",fib(10)); // funcionou
+	printf("%i\n",fib(11)); // funcionou
+}
---
+++ 1/padrao de testes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//funçao abaixo nao copie acima
+
+//funçao acima nao copie abaixo
+
+int main(){ //casos testes
+	printf("%i\n",mult(1,2)); // funcionou
+	printf("%i\n",mult(0,1)); // funcionou
+	printf("%i\n",mult(2,5)); // funcionou
+	printf("%i\n",mult(0,0)); // funcionou
+	printf("%i\n",mult(1,1)); // funcionou
+	printf("%i\n",mult(100,1000)); // funcionou
+}
