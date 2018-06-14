e0c7b04f48965d760666da4f52fa24d804396e5d
DemonLord <caiocsmuniz1@gmail.com>
2018-06-14 13:33:00 -0300

    questao 1e

---
+++ 1/1e.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//funçao abaixo nao copie acima
+void getPares (int n)
+{
+  if(n == 0)
+    printf("0\n");
+  else if (n % 2 == 0){
+    printf("%d ", n);
+    getPares(n - 1);
+  } else {
+    getPares(n - 1);
+  }
+}
+//funçao acima nao copie abaixo
+
+int main(){ //casos testes
+  getPares(5);    //funcionou
+  getPares(10);   //funcionou
+  getPares(50);   //funcionou
+  getPares(3);    //funcionou
+  getPares(2);    //funcionou
+  getPares(4);    //funcionou
+}
