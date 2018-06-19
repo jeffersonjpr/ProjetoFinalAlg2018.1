d05346d19624c5bfda761bff939e1af036525f3f
jeffersonjpr <jeffersonjpr>
2018-06-19 07:14:12 -0300

    2.c

---
+++ 3/dobra.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int n,m,nv[17],mv[17];
+	scanf(" %i",&n);
+	for(int i = 0;i<n;i++){
+		scanf(" %i",&nv[i]);
+	}
+	scanf(" %i",&m);
+	for(int i = 0;i<m;i++){
+		scanf(" %i",&mv[i]);
+	}
+	
+}
