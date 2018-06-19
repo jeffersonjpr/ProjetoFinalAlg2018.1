56e53fbabab43fc8490944212a19bac76125b763
Jefferson Michael <jefferson_michael407@hotmail.com>
2018-06-19 18:10:34 -0300

    hue

--- 3/dobra.c
+++ 3/dobra.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+void na() {printf("N\n");}
+void si() {printf("S\n");}
 int main(){
-	int n,m,nv[17],mv[17];
+	int n,m,nv[15],mv[15],teste1 = 0,teste2 = 0;
 	scanf(" %i",&n);
 	for(int i = 0;i<n;i++){
 		scanf(" %i",&nv[i]);
+		teste1+=nv[i];
 	}
 	scanf(" %i",&m);
 	for(int i = 0;i<m;i++){
 		scanf(" %i",&mv[i]);
+		teste2+=mv[i];
 	}
-	
+
+	//se a soma de todos os elementos forem diferentes, nao ha solução
+	// se a segunda fita for maior que a primeira nao ha soluçao
+	if(teste1 != teste2 || n < m) {
+		na();
+		return 0;
+	}
+
+
 }
